99f95260426fa3305cf71943bcd7ca5f3bb43afc
woojunchoi <tim.woojune1@gmail.com>
2018-03-12 01:39:57 -0700

    cart adding function with cookie completed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/products', (req,res) => {
     for(let name in products) {
         output += 
         `<li>
-       <a href='/cart/${name}'> ${products[name].title}
+       <a href="/cart/${name}"> ${products[name].title}</a>
         </li>`
     }
     res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart">Cart</a>`)
@@ -23,8 +23,8 @@ app.get('/products', (req,res) => {
 app.get('/cart/:id', (req,res) => {
     let id = req.params.id;
     let cart;
-    if(req.cookies.count) {
-        cart = req.cookies.count;
+    if(req.cookies.cart) {
+        cart = req.cookies.cart;
     }
     else {
         cart = {};
@@ -33,24 +33,37 @@ app.get('/cart/:id', (req,res) => {
     if(!cart[id]) {
         cart[id] = 0;
     }
-
-    cart[id] = parseInt(cart[id])+1;
+    cart[id] = parseInt(cart[id])+1
     res.cookie('cart',cart);
+    // res.send(cart)
     res.redirect('/cart')
 })
 
-app.get('/count', (req,res) => {
-    let count;
-    if(req.cookies.count) {
-        count = parseInt(req.cookies.count)
+app.get('/cart', (req,res) => {
+    let cart = req.cookies.cart;
+    let output =''
+    if(!cart) {
+        res.send('empty')
     }
     else {
-        count = 0;
+        for(let id in cart) {
+            output += `<li>${products[id].title}(${cart[id]})`
+        }
     }
-    count = count + 1
-    res.cookie('count', count)
-    res.send('count:' + count)
+    res.send(`<h1>CART</h1><ul>${output}</ul> <a href="/products">back to product list</a>`)
 })
+
+// app.get('/count', (req,res) => {
+//     let count;
+//     if(req.cookies.count) {
+//         count = parseInt(req.cookies.count)+1
+//     }
+//     else {
+//         count = 0;
+//     }
+//     res.cookie('count', count)
+//     res.send('count:' + count)
+// })
 app.listen(3000, () => {
     console.log('listening to 3000')
 })
