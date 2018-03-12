654f7960e789ab8d9838c6cdebe2290eeaa4c284
woojunchoi <tim.woojune1@gmail.com>
2018-03-11 23:58:58 -0700

    cookie

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express();
 const cookieParser = require('cookie-parser')
 
+app.use(cookieParser());
+
 app.get('/count', (req,res) => {
-    res.send('count: ')
+    let count;
+    if(req.cookies.count) {
+        count = parseInt(req.cookies.count)
+    }
+    else {
+        count = 0;
+    }
+    count = count + 1
+    res.cookie('count', count)
+    res.send('count:' + count)
 })
 app.listen(3000, () => {
     console.log('listening to 3000')
