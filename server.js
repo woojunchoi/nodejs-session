d8ae1bd7e4646f86460e9c1d110fb70eba9edf84
woojunchoi <tim.woojune1@gmail.com>
2018-03-12 00:27:14 -0700

    shopping cart

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const cookieParser = require('cookie-parser')
 
 app.use(cookieParser());
 
+const products = {
+    1:{title:'the history of web'},
+    2:{title:'the next web'}
+}
+
+app.get('/products', (req,res) => {
+    let output = ''
+    for(let name in products) {
+        output += 
+        `<li>
+       <a href='/cart/${name}'> ${products[name].title}
+        </li>`
+    }
+    res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart">Cart</a>`)
+})
+
 app.get('/count', (req,res) => {
     let count;
     if(req.cookies.count) {
