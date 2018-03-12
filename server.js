937b233f6500ae89a9abc6e430c1b23ff4c34d77
woojunchoi <tim.woojune1@gmail.com>
2018-03-11 23:36:02 -0700

    cookie parser added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express();
+const cookieParser = require('cookie-parser')
 
+app.get('/count', (req,res) => {
+    res.send('count: ')
+})
 app.listen(3000, () => {
     console.log('listening to 3000')
 })
